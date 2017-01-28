75b0f96b6738f09c123b2158a02a903acaa1b9ab
jhoelter <jhoelter@paypal.com>
2017-01-27 19:18:45 -0500

    Chapter 7 answers

--- src/chapter7.clj
+++ src/chapter7.clj
@@ -6,7 +6,14 @@
 ; favorite sci-fi movie.
 
 
+; (eval (quote (list (read-string "Jeff") (read-string "Star-Wars"))))
+(eval (list (read-string "print") "Jeff:" "Star Wars\n"))
 
 
+; 2. Create an infix function that takes a list like 
+; (1 + 3 * 4 - 5) and trans- forms it into the lists 
+; that Clojure needs in order to correctly evaluate 
+; the expression using operator precedence rules.
 
-; 2. Create an infix function that takes a list like (1 + 3 * 4 - 5) and trans- forms it into the lists that Clojure needs in order to correctly evaluate the expression using operator precedence rules.
+(let [infix (read-string "(1 + 3 * 4 - 5)")]
+  (list (second infix) (first infix) (last infix)))
