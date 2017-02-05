6b26c4fb6ef333b1ac46a9be47616f5857818e6d
jhoelter <jeffh@loar.net>
2017-02-05 14:18:57 -0500

    Chapter 9 completed.

--- src/chapter9.clj
+++ src/chapter9.clj
@@ -4,33 +4,46 @@
 ; it on Bing and Google using the slurp function. Your function should 
 ; return the HTML of the first page returned by the search.
 
-(def gimli-headshots ["serious.jpg" "fun.jpg" "playful.jpg"])
-(defn email-user
-  [email-address]
-  (println "Sending headshot notification to" email-address))
-(defn upload-document
-  "Needs to be implemented"
-  [headshot]
-  true)
-(let [notify (delay (email-user "and-my-axe@gmail.com"))] (doseq [headshot gimli-headshots]
-(future (upload-document headshot) (force notify))))
+(defn offline-slurp
+  [search-engine search-term]
+    (str search-engine search-term))
 
+(def bing-and-google-search-engines ["http://www.bing.com/search?q=" "https://www.google.com/#q="])
 
-
-(def search-engines ["https://google.com" "https://bing.com"])
 
 (defn search-bing-and-google
   [search-term]
   (let [search-promise (promise)]
-    (doseq [search-engine search-engines]
-      (future (deliver search-promise (slurp search-engine search-term))))
-    (@search-promise)))
+    (doseq [search-engine bing-and-google-search-engines]
+      (future (deliver search-promise (slurp (str search-engine search-term)))))
+    (str @search-promise)))
+
+; (search-bing-and-google "water")
 
 
 ; 2. Update your function so it takes a second argument consisting of 
 ; the search engines to use.
 
+(defn search-with-search-engines
+  [search-term search-engines]
+  (let [search-promise (promise)]
+    (doseq [search-engine search-engines]
+      (future (deliver search-promise (slurp (str search-engine search-term)))))
+    (str @search-promise)))
+
+; (search-with-search-engines "water" bing-and-google-search-engines)
+
 
 ; 3. Create a new function that takes a search term and search engines 
 ; as arguments, and returns a vector of the URLs from the first page of 
 ; search results from each search engine.
+
+(defn search-for-urls
+  [search-term search-engines]
+  (let [search-promise (promise)]
+    (doseq [search-engine search-engines]
+      (future (deliver search-promise (slurp (str search-engine search-term)))))
+    (take 10 (re-seq #"(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"
+        (str @search-promise)))))
+
+; (search-for-urls "water" bing-and-google-search-engines)
