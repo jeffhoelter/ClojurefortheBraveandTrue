5e757d61a300369124df955ad608ac4c6b28f6c2
jeffhoelter <jeffh@loar.net>
2017-02-12 07:59:11 -0500

    Chapter10 notes and scratch work.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] [org.clojure/tools.nrepl "0.2.12" :scope "test"]]
   :main ^:skip-aot chapter10
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chapter10.clj
+++ src/chapter10.clj
@@ -1,15 +1,50 @@
 (ns chapter10)
 
 (def sock-varieties
-	#{"short" "tall" "brown" "red"})
+  #{"short" "tall" "brown" "red"})
 
 (defn sock-count
-	[sock-variety count]
-	{:variety sock-variety
-		:count count})
+  [sock-variety count]
+  {:variety sock-variety
+   :count count})
 
 (defn generate-sock-gnome
-	[name]
-	{:name name
-		:socks #{}})
+  [name]
+  {:name name
+   :socks #{}})
 
+(def sock-gnome (ref (generate-sock-gnome "Barumpharumph")))
+
+(def dryer (ref {:name "LG 1337"
+                 :socks (set (map #(sock-count % 2) sock-varieties))}))
+
+(defn steal-sock
+  [gnome dryer]
+  (dosync
+   (when-let [pair (some #(if (= (:count %) 2) %) (:socks @dryer))]
+     (let [updated-count (sock-count (:variety pair) 1)]
+       (alter gnome update-in [:socks] conj updated-count)
+       (alter dryer update-in [:socks] disj pair)
+       (alter dryer update-in [:socks] conj updated-count)))))
+
+(steal-sock sock-gnome dryer)
+
+(:socks @sock-gnome)
+
+
+(defn similar-socks
+  [target-sock sock-set]
+  (filter #(= (:variety %) (:variety target-sock)) sock-set))
+
+(similar-socks (first (:socks @sock-gnome)) (:socks @dryer))
+
+
+(defn sleep-print-update
+  [sleep-time thread-name update-fn]
+  (fn [state]
+    (Thread/sleep sleep-time)
+    (println (str thread-name ": " state))
+    (update-fn state)))
+(def counter (ref 0))
+(future (dosync (commute counter (sleep-print-update 100 "Thread A" inc))))
+(future (dosync (commute counter (sleep-print-update 150 "Thread B" inc))))
