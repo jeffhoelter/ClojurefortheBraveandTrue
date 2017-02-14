e40f6066f51892431d905be16c1da0a1130b9523
jeffhoelter <jeffh@loar.net>
2017-02-14 15:52:11 -0500

    Chapter 10 in progress.

--- src/chapter10.clj
+++ src/chapter10.clj
@@ -43,8 +43,55 @@
   [sleep-time thread-name update-fn]
   (fn [state]
     (Thread/sleep sleep-time)
-    (println (str thread-name ": " state))
+    ;; (println (str thread-name ": " state))
     (update-fn state)))
 (def counter (ref 0))
 (future (dosync (commute counter (sleep-print-update 100 "Thread A" inc))))
 (future (dosync (commute counter (sleep-print-update 150 "Thread B" inc))))
+
+
+;; Exercises
+
+;; 1. Create an atom with the initial value 0, use swap! to
+;; increment it a couple of times, and then dereference it.
+
+(def counter-atom (atom 0))
+(swap! counter-atom inc)
+(swap! counter-atom inc)
+(swap! counter-atom inc)
+@counter-atom
+
+
+;; 2. Create a function that uses futures to parallelize the
+;; task of download- ing random quotes from
+;; http://www.braveclojure.com/random-quote using
+;; (slurp "http://www.braveclojure.com/random-quote"). The
+;; futures should update an atom that refers to a total word
+;; count for all quotes. The function will take the number of
+;; quotes to download as an argument and return the atom’s final
+;; value. Keep in mind that you’ll need to ensure that all
+;; futures have finished before returning the atom’s final
+;; value. Here’s how you would call it and an example result:
+;;                 (quote-word-count 5)
+;;                 ; => {"ochre" 8, "smoothie" 2}
+
+(slurp "http://www.braveclojure.com/random-quote")
+
+(defn slurp-brave-into-frequencies-map
+  []
+  (frequencies (str/split (slurp "http://www.braveclojure.com/random-quote") #" ")))
+
+(defn quote-word-count
+  [count]
+  (repeatedly count (slurp-brave-into-frequencies-map)))
+(quote-word-count 5)
+
+
+
+
+
+;; 3. Create representations of two characters in a game. The
+;; first character has 15 hit points out of a total of 40. The
+;; second character has a healing potion in his inventory.
+;; Use refs and transactions to model the consumption of the
+;; healing potion and the first character healing.
