8a045a791545bb8c39b624dd9aacd979fbcb5843
jeffhoelter <jeffh@loar.net>
2017-02-21 21:29:07 -0500

    Chapter 11 sample project.

---
+++ src/chapter11/playsync/src/playsync/core.clj
@@ -0,0 +1,5 @@
+(ns playsync.core
+  (:require [clojure.core.async
+             :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread
+                     alts! alts!! timeout]]))
