5a9e2dd9dee5b957b40142ddda4e611f78750d72
Hoelter <jhoelter@dm-bwi-21000207.paypalcorp.com>
2017-02-28 16:00:00 -0500

    Chapter 13 setup.

---
+++ src/chapter13.clj
@@ -0,0 +1,12 @@
+(ns chapter13)
+
+
+;; 1. Extend the full-moon-behavior multimethod to add behavior for your own kind of were-creature.
+
+;; 2. Create a WereSimmons record type, and then extend the WereCreature protocol.
+
+;; 3. Create your own protocol, and then extend it using extend-type and extend-protocol.
+
+;; 4. Create a role-playing game that implements behavior using multiple dispatch.
+
+
